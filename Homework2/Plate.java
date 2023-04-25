4d1e900728c6d6e49767f1931eef18979460cc19
AlekhinVladimir <Alehin.v.m@mail.com>
2023-04-25 11:01:15 +0400

    hw2

--- Homework2/Plate.java
+++ Homework2/Plate.java
@@ -1,4 +1,4 @@
-package OOP.Homework2;
+package Homework2;
 
 public class Plate {
     private int food;
@@ -20,3 +20,39 @@ public class Plate {
         System.out.printf("!!!Add %d foods\n\n", add);
     }
 }
+// Итак, базовый класс для тарелки с едой.
+// Тут нам необходимо разработать два метода для добавления еды и для удаления еды (при кормлении животного)
+
+// /**
+// * Тарелка с едой
+// */
+// public class Plate {
+
+//     private int food;
+
+//     /**
+//      * Добавление еды в тарелку
+//      * @param amount  - кол-во еды (в условных единицах)
+//      */
+//     public void putFood(int amount) {
+//             this.food += amount;
+//             System.out.printf("Добавлено %d еды в тарелку, теперь в тарелке %d еды\n", amount, food);
+//     }
+
+//     /**
+//      * Удаление еды из тарелки
+//      * @param amount - кол-во еды (в условных единицах)
+//      * @return - если в тарелке, после удаления, остается достаточно еды, возвращаем результат операции true,
+//      * в противном случае, не удаляем еду, возвращаем результат операции false
+//      */
+//     public boolean decreaseFood(int amount) {
+//             if (this.food < amount) {
+//                     System.out.println("Недостаточно еды для ее удаления");
+//                     return false;
+//             }
+//             this.food -= amount;
+//             System.out.printf("Удалено %d еды из тарелки, теперь в тарелке %d еды\n", amount, food);
+//             return true;
+//     }
+
+// }
